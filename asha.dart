ffa6e2b66650c24d8fd09a46609fe397fc22f864
asha <ashakisonga2022@gmail.com>
2024-02-26 14:37:06 +0300

    Dart data types

---
+++ asha.dart
@@ -0,0 +1,25 @@
+void main() {
+  /* this for multiple
+   Int data type*/
+  int score = 100;
+
+  // Double data type
+  double temperature = 20.5;
+
+  // String data type
+  String message =
+      "Hello World!, I'm Asha from Tanzania, A Data Scientists and ML Developer";
+
+  // List data type
+  List<String> fruits = ['apple', 'banana', 'orange'];
+
+  // Map data type
+  Map<String, int> ages = {'Alice': 25, 'Bob': 30, 'Charlie': 35};
+
+  // Print data stored in each data type
+  print("Score: $score");
+  print("Temperature: $temperature");
+  print("Message: $message");
+  print("Fruits: $fruits");
+  print("Ages: $ages");
+}
